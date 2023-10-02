555b83a493446cff67a261ff6b72b7676e416cb5
Jack Keane <jwk324@lehigh.edu>
2023-10-02 19:40:35 -0400

    new build

--- my-app/src/setupProxy.js
+++ my-app/src/setupProxy.js
@@ -1,20 +1,20 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://thawing-savannah-53116-6c6cf43d1486.herokuapp.com/',
-      changeOrigin: true,
-    })
-  );
+// module.exports = function(app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'https://thawing-savannah-53116-6c6cf43d1486.herokuapp.com/',
+//       changeOrigin: true,
+//     })
+//   );
 
-  app.use(
-    '/',
-    createProxyMiddleware({
-      target: 'https://thawing-savannah-53116-6c6cf43d1486.herokuapp.com/',
-      changeOrigin: true,
-    })
-  );
-};
+//   app.use(
+//     '/',
+//     createProxyMiddleware({
+//       target: 'https://thawing-savannah-53116-6c6cf43d1486.herokuapp.com/',
+//       changeOrigin: true,
+//     })
+//   );
+// };
 
